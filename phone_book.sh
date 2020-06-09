901984d55c5c270a66c722b364939bf3922029cf
DavidChatak <63158138+DavidChatak@users.noreply.github.com>
2020-06-09 20:46:06 +0300

    Update phone_book.sh

--- phone_book.sh
+++ phone_book.sh
@@ -6,6 +6,7 @@ if [ $? != 0 ] ;
     echo "You are not member of sudoers, dude $whoami" 
 
 fi
+
 control=$(find . -name AccessLog.txt | wc -l)
 if [ $control = 0 ]; then
 echo "There is no file of AccessLog.txt "
@@ -22,14 +23,12 @@ if [ $control = 0 ]; then
     echo "There is NO Python3....."
     echo "installing Python3...."
     sleep 1
-    #sudo yum install -y python3
+    sudo yum install -y python3
     echo "done...."
-    sleep 1
 else
     echo "There is already PYTHON3 installed...."
 fi
-currentdir=$( pwd )
-p=$( echo $PATH )
+
 pp="/usr/bin/python3"
 # IFS=': ' read -r -a array <<< "$p"
 ll=$(echo $PATH | grep $pp | wc -l )
@@ -39,8 +38,9 @@ if  [[ $ll != 0 ]]
     echo "$pp in PATH"
     else
     echo "$pp is not in PATH, adding..."
-    sleep 1
-    export PATH=$PATH:$pp
+    sleep 2
+    echo "export PATH=$PATH:/usr/bin/python3" >> ~/.bashrc
+    
 fi
 # git control
 git_control=$(sudo yum list installed git| wc -l)
@@ -57,12 +57,9 @@ then
 echo "phone.py exist"
 else
 echo "phone.py does not exist"
-sudo wget https://github.com/DavidChatak/case_study/blob/master/phone.py
 fi
 echo "phone_book.py running"
 sleep 1
 
 
 python3 phone.py
-
-# /home/ec2-user:/home/ec2-user/.vscode-server/bin/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/bin:/home/ec2-user/.vscode-server/bin/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/usr/bin/python3
