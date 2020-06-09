bc6302383b8a14def4aa59a1dba709461b0bd467
DavidChatak <63158138+DavidChatak@users.noreply.github.com>
2020-06-09 20:10:24 +0300

    Update phone_book.sh

--- phone_book.sh
+++ phone_book.sh
@@ -1,82 +1,68 @@
 #!/bin/bash
-while True
-do
-    #satirsay=$(cat foto.csv | wc -l )
-    #i=4
-    #harfsay=$(cat foto.csv | grep -w '1satir' | wc -c )
-    #echo $harfsay $satirsay
-    Klasor="foto-test"
-    strno=$( cat foto.csv | wc -l ) 
-	for (( i=1; i<=$strno; i++ ))
-	do
-		ad=$( sed -n "$i"p foto.csv | cut -f1 -d ";" )
-		ad2=$( sed -n "$i"p foto.csv | cut -f2 -d ";" )
-        echo "$ad -------------------------"
-        
-		if [ -e "$Klasor/$ad" ]
-		then
-            echo "doğrüüüüüüüüüüüü"
-			if [ -e "$Klasor/newfoto/$ad2" ]
-			then
-				jpg=$( echo $ad2 | cut -d "." -f2 )
-				asil=$( echo $ad2 | cut -d "." -f1 )
-				say=$( ls $Klasor/newfoto/ | grep $asil | wc -l )
-				kopy="$asil Copy($say) .$jpg"
-                echo $kopy
-                echo $Klasor/$kopy
-            else
-                echo burda
-            fi
-        fi
-    done
-    for file in $( ls foto-test )
-	do
-		filex=$( echo $file | cut -d "_" -f1 )
-        echo $filex
-    done
-    ad=$(cat foto.csv | cut -d ";" -f 3)
-    uniqsayi=$( ls foto-test/ | cut -d "_" -f1| uniq | wc -l )
-    uniq=$( ls foto-test/ | cut -d "_" -f1| uniq >> uniq.csv )
-    echo $uniqsayi
-    echo $uniq
-    echo "Bash version ${BASH_VERSION}..."
-    for (( i=1; i<=$uniqsayi; i++ ))
-    do 
-     echo "Welcome $i times"
-    done
-	for (( i=1; i<=$uniqsayi; i++ ))
-	do
-    echo " gel $i"
-    item=$( sed -n "$i"p uniq.csv )
-    echo $item
-    done
-    read -p "Devam edilsin mi? (y/n): " onay
-    if [ $onay == "y" ]
+clear
+sudo echo "welcome $USER"
+if [ $? != 0 ] ;
     then
-            echo "Devam ediliyor."
-    else
-            break
-    fi
+    echo "You are not member of sudoers, dude $whoami" 
 
-    for a in $ad
-    do
-        echo $a
-        ad1=$( cat foto.csv | grep -w $a | cut -f1 -d ";" )
-        ad2=$( cat foto.csv | grep -w $a | cut -f2 -d ";" )
-        jpg=$( echo $ad2 | cut -d "." -f2 )
-        asil=$( echo $ad2 | cut -d "." -f1 )
-        say=1 #$( ls $Klasor/newfoto/ | grep $asil | wc -l )
-        kopy="$asil copy($say) .$jpg"
-        echo $ad2
-        echo " $jpg  ve  $asil "
-        echo " kopyası $kopy "
-        read -p "Devam edilsin mi? (y/n): " onay
-        if [ $onay == "y" ]
-        then
-            echo "Devam ediliyor."
-        else
-            break
-        fi
-    # echo  " bu ne ya $a -- $ad1---  $ad2 "
-    done
-done
+fi
+control=$(find . -name AccessLog.txt | wc -l)
+if [ $control = 0 ]; then
+echo "There is no file of AccessLog.txt "
+echo "Creating AccessLog.txt ..... "
+sleep 2
+sudo touch AccessLog.txt
+sudo chmod 777 AccessLog.txt
+fi
+num=$(cat AccessLog.txt | wc -l )
+date=$( date )
+sudo echo "$(($num+1)) $USER $date"  >> AccessLog.txt
+control=$( sudo yum list installed python3 | wc -l )
+if [ $control = 0 ]; then
+    echo "There is NO Python3....."
+    echo "installing Python3...."
+    sleep 1
+    #sudo yum install -y python3
+    echo "done...."
+    sleep 1
+else
+    echo "There is already PYTHON3 installed...."
+fi
+currentdir=$( pwd )
+p=$( echo $PATH )
+pp="/usr/bin/python3"
+# IFS=': ' read -r -a array <<< "$p"
+ll=$(echo $PATH | grep $pp | wc -l )
+
+if  [[ $ll != 0 ]]
+    then
+    echo "$pp in PATH"
+    else
+    echo "$pp is not in PATH, adding..."
+    sleep 1
+    export PATH=$PATH:$pp
+fi
+# git control
+git_control=$(sudo yum list installed git| wc -l)
+if [ git_control > 0 ]
+then 
+echo "Git installed already"
+else
+sudo yum install git -y 
+fi
+#phone_book.py control
+
+if [ -e phone.py ]
+then
+echo "phone.py exist"
+else
+echo "phone.py does not exist"
+sudo wget https://github.com/DavidChatak/case_study/blob/master/phone.py
+fi
+echo "phone_book.py running"
+sleep 1
+
+
+python3 phone.py
+
+# /home/ec2-user:/home/ec2-user/.vscode-server/bin/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/bin:/home/ec2-user/.vscode-server/bin/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin:/usr/bin/python3
